babfe636d9d7259cd3c6bbd33debc8e03c1fed5a
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2019-02-21 12:52:10 -0500

    first commit

---
+++ Assets/Scenes/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	private bool isPressed = false;
+
+	public float releaseTime = .15f; 
+
+	void Update ()
+	{
+		if (isPressed)
+		{
+			rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	void OnMouseDown ()
+	{
+		isPressed = true;
+		rb.isKinematic = true;
+	}
+
+
+
+	void OnMouseUp ()
+	{
+		isPressed = false;
+		rb.isKinematic = false;
+		StartCoroutine(Release());
+	}
+
+	IEnumerator Release(){
+
+		yield return new WaitForSeconds(releaseTime) ;
+		GetComponent<SpringJoint2D>().enabled = false;
+		this.enabled = false;
+	}
+
+}
+
---
+++ Assets/startGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    public string levels;
+    public void startBtn(){
+    SceneManager.LoadScene ("Level");
+
+    }
+
+public void LevelReset(){
+
+SceneManager.LoadScene ("Level");
+ }
+
+ public void LeveloneReset(){
+
+SceneManager.LoadScene ("Level1");
+ }
+
+ public void LevelTwoReset(){
+
+SceneManager.LoadScene ("Level2");
+ }
+
+//   public void LevelThreeReset(){
+
+// SceneManager.LoadScene ("Level2");
+//  }
+
+
+
+
+    
+    }
+
